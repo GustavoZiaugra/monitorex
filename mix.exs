eed32f99ab537ab6b0f456cdae89a7df9313f056
Gustavo Ziaugra <gustavo.ziaugra@live.com>
2026-05-13 18:29:55 +0000

    release: prepare v0.5.0 — CHANGELOG, version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Monitorex.MixProject do
   def project do
     [
       app: :monitorex,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
